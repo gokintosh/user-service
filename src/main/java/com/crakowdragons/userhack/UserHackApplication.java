e61d5301b74ffef913b66127ab81110ca5330d63
gokintosh <gokul.kannambally_balachandran@student.po.edu.pl>
2022-11-01 13:07:16 +0100

    add cors config

--- src/main/java/com/crakowdragons/userhack/UserHackApplication.java
+++ src/main/java/com/crakowdragons/userhack/UserHackApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class UserHackApplication {
@@ -16,6 +18,17 @@ public class UserHackApplication {
 	@Bean
 	PasswordEncoder passwordEncoder(){
 		return new BCryptPasswordEncoder();
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer(){
+		return new WebMvcConfigurer(){
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("http://localhost:4200");
+			}
+		};
 	}
 
 }
